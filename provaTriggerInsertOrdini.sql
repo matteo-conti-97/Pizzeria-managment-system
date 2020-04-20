a5dd1890067a7f83dc4aaeddbb70653dd0655151
contiinguniroma2 <56634851+contiinguniroma2@users.noreply.github.com>
2020-04-20 22:21:58 +0200

    sd

---
+++ provaTriggerInsertOrdini.sql
@@ -0,0 +1,8 @@
+insert into mydb.ingrediente values('funghi',3,0.5);
+insert into mydb.ingrediente values('pomodori',0,0.5);
+insert into mydb.pizza values('margherita',5,4.5);
+insert into mydb.pizza values('boscaiola',0,6.0);
+insert into mydb.bevanda values('peroni',0,1.5);
+insert into mydb.bevanda values('coca-cola',5,1.0);
+insert into mydb.tavolo values(3,2,null);
+insert into mydb.scontrino (Data, ScontrinoTavolo) values(now(),3);
