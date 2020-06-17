9c75f364a48c4fb0b8cebcb4a4e508c6a957583c
Matteo <matteo.conti.97@students.uniroma2.eu>
2020-06-18 00:18:16 +0200

    Database
    -Corrette query visualizza ordini espletati
    -Cambiati double in float

--- provaTriggerInsertOrdini.sql
+++ provaTriggerInsertOrdini.sql
@@ -12,10 +12,13 @@ insert into dbPizzeria.Tavolo values(3,2,null);
 insert into dbPizzeria.Tavolo values(4,2,null);
 call dbPizzeria.registra_cliente('1234', 'matteo', 'conti', 2);
 call dbPizzeria.assegna_tavolo_a_cliente('1234', 3);
-insert into dbPizzeria.scontrino (Data, ScontrinoTavolo) values(now(),3);
-insert into dbPizzeria.OrdinePizza (Pizza,Tavolo) values ('margherita',3);
-insert into dbPizzeria.PizzaPlus (Nome,Base) values ('margheritaFunghi','margherita');
-insert into dbPizzeria.Aggiunta (PizzaPlus,Ingrediente) values ('margheritaFunghi', 'funghi');
-insert into dbPizzeria.OrdinePizzaPlus (PizzaPlus,Tavolo) values ('margheritaFunghi',3);
-insert into dbPizzeria.OrdineBevanda (Bevanda,Tavolo) values ('coca-cola',3);
-insert into dbPizzeria.OrdineBevanda (Bevanda,Tavolo) values ('coca-cola',4);
+call dbPizzeria.aggiungi_impiegato(1, 'asda', 'asda', 'manager', 1);
+call dbPizzeria.aggiungi_impiegato(2, 'asda', 'asda', 'cameriere', 2);
+call dbPizzeria.aggiungi_impiegato(3, 'asda', 'asda', 'pizzaman', 3);
+call dbPizzeria.aggiungi_impiegato(4, 'asda', 'asda', 'barman', 4);
+call dbPizzeria.assegna_tavolo_a_cameriere(2, 3);
+call dbPizzeria.registra_ordine_pizza(3, 'margherita');
+call dbPizzeria.registra_ordine_bevanda(3, 'coca-cola');
+call dbPizzeria.registra_ordine_pizza_plus(3, 'margherita', 'funghi', '', '', '', '');
+
+
